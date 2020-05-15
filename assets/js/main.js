4709d5d32cebd95dddeff8026ce2b3914ad069b5
Hoang Tran <minhhoang.tran.180186@gmail.com>
2020-05-15 20:10:39 +0300

    add svg icon system

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,22 @@
-window.addEventListener("load", () => {
+window.addEventListener("load", onload);
+document.querySelector(".header__toggler").addEventListener("click", toggle);
+
+
+function closePreloader() {
     let preloader = document.querySelector(".preloader");
     preloader.addEventListener("animationend", () => {
-        preloader.classList.remove("closing");
-        preloader.classList.add("closed");
+        preloader.classList.replace("-hiding", "-hidden");
     });
-    preloader.classList.add("closing");
-});
+    preloader.classList.add("-hiding");
+}
+
+function onload() {
+    closePreloader();
+}
+
+function toggle(event) {
+    let toggler = event.currentTarget;
+    toggler.classList.toggle("-active");
+    toggler.parentNode.querySelector(".header__nav").classList.toggle("-open");
+    toggler.parentNode.querySelector(".header__media").classList.toggle("-open");
+}
